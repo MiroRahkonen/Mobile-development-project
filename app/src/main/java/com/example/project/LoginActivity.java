ee01f310d07efdd21a661e844451868cddc733a6
miro <miro.rahkonen.000@gmail.com>
2024-03-04 11:50:12 +0200

    Finished login and register user interfaces Updated code comments

--- app/src/main/java/com/example/project/LoginActivity.java
+++ app/src/main/java/com/example/project/LoginActivity.java
@@ -32,9 +32,12 @@ public class LoginActivity extends AppCompatActivity implements TextWatcher {
 
         username_tv = (TextView) findViewById(R.id.login_username_tv);
         password_tv = (TextView) findViewById(R.id.login_password_tv);
+
+        //Add TextViews onto TextWatcher
         username_tv.addTextChangedListener(this);
         password_tv.addTextChangedListener(this);
 
+        //Clicking login button
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,8 +47,9 @@ public class LoginActivity extends AppCompatActivity implements TextWatcher {
                 startActivity(mainIntent);
             }
         });
-        login_button.setClickable(false);
+        disableLoginButton(); //On startup make login unclickable
 
+        // Clicking register button, redirects to register account screen
         goto_register_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,35 +57,42 @@ public class LoginActivity extends AppCompatActivity implements TextWatcher {
                 startActivity(registerIntent);
             }
         });
-
-
     }
 
-    protected void checkInputs(String username,String password){
-        // If input is valid, make login clickable and change button colors
-        if(username.length() >= 4 && password.length() >= 8){
-            login_button.setClickable(true);
-            login_button.setBackgroundTintList(getColorStateList(R.color.secondary));
-            login_button.setTextColor(getColor(R.color.white));
-        }else{
-            login_button.setClickable(false);
-            login_button.setBackgroundTintList(getColorStateList(R.color.inactive));
-            login_button.setTextColor(getColor(R.color.black));
-        }
+    protected void disableLoginButton(){
+        login_button.setClickable(false);
+        login_button.setBackgroundTintList(getColorStateList(R.color.inactive));
+        login_button.setTextColor(getColor(R.color.black));
     }
 
-    // Help with TextWatcher from this article https://dzone.com/articles/how-to-monitor-textview-changes-in-android
+    protected void enableLoginButton(){
+        login_button.setClickable(true);
+        login_button.setBackgroundTintList(getColorStateList(R.color.secondary));
+        login_button.setTextColor(getColor(R.color.white));
+    }
+
+    // TextWatcher tutorial from this article https://dzone.com/articles/how-to-monitor-textview-changes-in-android
+    // TextWatcher functions
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
     }
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
+        // Checking that every input is valid
         String username = username_tv.getText().toString();
+        if(username.length() < 4){
+            disableLoginButton();
+            return;
+        }
         String password = password_tv.getText().toString();
-        checkInputs(username,password);
+        if(password.length() < 8){
+            disableLoginButton();
+            return;
+        }
+        // If all checks pass, enable login button
+        enableLoginButton();
     }
     @Override
     public void afterTextChanged(Editable s) {
-
     }
 }
--- app/src/main/java/com/example/project/RegisterActivity.java
+++ app/src/main/java/com/example/project/RegisterActivity.java
@@ -3,29 +3,44 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements TextWatcher {
+
+    protected TextView username_tv;
+    protected TextView email_tv;
+    protected TextView password_tv;
+    protected Button register_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        //Changing appbar title to register activity
         TextView title_tv = (TextView) findViewById(R.id.title_tv);
         ImageButton appbar_back_button = (ImageButton) findViewById(R.id.back_button);
-        Button register_button = (Button) findViewById(R.id.register_account_button);
-        TextView username_tv = (TextView) findViewById(R.id.register_username_tv);
-        TextView email_tv = (TextView) findViewById(R.id.register_email_tv);
-        TextView password_tv = (TextView) findViewById(R.id.register_password_tv);
+        register_button = (Button) findViewById(R.id.register_account_button);
+        username_tv = (TextView) findViewById(R.id.register_username_tv);
+        email_tv = (TextView) findViewById(R.id.register_email_tv);
+        password_tv = (TextView) findViewById(R.id.register_password_tv);
 
+        //Add TextViews into TextWatcher
+        username_tv.addTextChangedListener(this);
+        email_tv.addTextChangedListener(this);
+        password_tv.addTextChangedListener(this);
+
+        //Changing appbar title to register activity, make back button visible
         title_tv.setText(getString(R.string.title_register));
         appbar_back_button.setVisibility(View.VISIBLE);
 
+        // Create listener for back button
         appbar_back_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,6 +48,7 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+        //Create listener for register button
         register_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,6 +58,54 @@ public class RegisterActivity extends AppCompatActivity {
                 finish();
             }
         });
+        disableRegisterButton(); //On startup make register button unclickable
+    }
+
+    protected void disableRegisterButton(){
         register_button.setClickable(false);
+        register_button.setBackgroundTintList(getColorStateList(R.color.inactive));
+        register_button.setTextColor(getColor(R.color.black));
+    }
+
+    protected void enableRegisterButton(){
+        register_button.setClickable(true);
+        register_button.setBackgroundTintList(getColorStateList(R.color.secondary));
+        register_button.setTextColor(getColor(R.color.white));
+    }
+
+    // TextWatcher tutorial from this article https://dzone.com/articles/how-to-monitor-textview-changes-in-android
+    // Required TextWatcher functions
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        // Checking that every input is valid
+        int username_length = username_tv.getText().toString().length();
+        if(username_length < 4){
+            disableRegisterButton();
+            return;
+        }
+
+        String email = email_tv.getText().toString();
+        boolean email_is_valid = Patterns.EMAIL_ADDRESS.matcher(email).matches();
+        if(!email_is_valid){
+            disableRegisterButton();
+            return;
+        }
+
+        int password_length = password_tv.getText().toString().length();
+        if(password_length < 8){
+            disableRegisterButton();
+            return;
+        }
+        // If all checks pass, enable register button
+        enableRegisterButton();
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
     }
 }
