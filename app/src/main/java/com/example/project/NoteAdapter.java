4c0a5bc2c68d3d6534fcf4c26691a76cf73622bf
miro <miro.rahkonen.000@gmail.com>
2024-03-08 15:41:43 +0200

    New functions into DBManager to handle deleting and adding notes, made Main screen RecyclerView update automatically when adding and removing notes

--- app/src/main/java/com/example/project/NoteAdapter.java
+++ app/src/main/java/com/example/project/NoteAdapter.java
@@ -1,10 +1,8 @@
 package com.example.project;
 
 import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
